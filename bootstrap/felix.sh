f25561b39e2dd2e87649a2e07967815f98baa95c
Dan Guest <dguest@cern.ch>
2015-10-06 13:46:45 -0700

    add more stuff (which does things I don't understand)

--- bootstrap/felix.sh
+++ bootstrap/felix.sh
@@ -53,3 +53,32 @@ fi
     RPMNAME=libpib-${VERSION}-1.el7.centos.x86_64.rpm
     sudo rpm -ihv ${HOME}/rpmbuild/RPMS/x86_64/${RPMNAME}
 )
+
+# pibnetd
+set -eu
+(
+    cd pib/pibnetd/
+    make
+    sudo install -m 755 -D pibnetd /usr/sbin/pibnetd
+    RC=/etc/rc.d/init.d/pibnetd
+    sudo install -m 755 -D scripts/redhat-pibnetd.init $RC
+)
+
+RPMDIR=pibnetd-0.4.6
+if [[ -d ${RPMDIR} ]] ; then
+    rm -rf $RPMDIR
+fi
+(
+    # If you want to create binary RPM file, input the following commands.
+    cp -r pib/pibnetd ${RPMDIR}
+    tar czvf ${HOME}/rpmbuild/SOURCES/${RPMDIR}.tar.gz ${RPMDIR}/
+    rpmbuild -bs pib/pibnetd/pibnetd.spec
+    SRPMNAME=${RPMDIR}-1.el7.centos.src.rpm
+    rpmbuild --rebuild ${HOME}/rpmbuild/SRPMS/${SRPMNAME}
+    RPMNAME=${RPMDIR}-1.el7.centos.x86_64.rpm
+    sudo rpm -ihv ${HOME}/rpmbuild/RPMS/x86_64/${RPMNAME}
+)
+
+# enable services
+sudo systemctl enable rdma.service
+sudo systemctl start rdma.service
