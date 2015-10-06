09244bbca952cb33cd5abfcc83a9005822ffa8a9
Dan Guest <dguest@cern.ch>
2015-10-06 12:52:26 -0700

    add a bunch of IB install stuff

--- bootstrap/basic.sh
+++ bootstrap/basic.sh
@@ -13,5 +13,14 @@ yum install -y tbb-devel
 yum install -y librdmacm-devel
 yum install -y net-tools
 yum install -y gdb
+
 # has to be built...
 # yum install -y libevent
+
+# stuff for IB emulator
+yum install -y kernel-devel
+yum install -y opensm-devel
+yum install -y libibverbs-utils
+yum install -y librdmacm-utils
+yum install -y infiniband-diags
+yum install -y rpm-build
--- bootstrap/felix.sh
+++ bootstrap/felix.sh
@@ -2,3 +2,50 @@
 set -eu
 
 svn co svn+ssh://dguest@svn.cern.ch/reps/FELIX/hostSoftware
+
+if [[ -d pib ]] ; then
+    rm -rf pib
+fi
+
+# get and install IB emulator
+git clone https://github.com/nminoru/pib.git
+(
+    cd pib/driver/
+    make
+    sudo make modules_install
+)
+
+# build the rpm
+if [[ -d rpmbuild ]] ; then
+    rm -rf rpmbuild
+fi
+(
+    VERSION=0.4.6
+    cp -r pib/driver pib-$VERSION
+    mkdir -p ${HOME}/rpmbuild/SOURCES/
+    tar czvf ${HOME}/rpmbuild/SOURCES/pib-${VERSION}.tar.gz pib-${VERSION}/
+    cp pib/driver/pib.conf ${HOME}/rpmbuild/SOURCES/
+    cp pib/driver/pib.files ${HOME}/rpmbuild/SOURCES/
+    rpmbuild -bs pib/driver/pib.spec
+
+    SRPMNAME=pib-${VERSION}-1.el7.centos.src.rpm
+    rpmbuild --rebuild ${HOME}/rpmbuild/SRPMS/${SRPMNAME}
+    RPMNAME=pib-debuginfo-${VERSION}-1.el7.centos.x86_64.rpm
+    sudo rpm -ihv ${HOME}/rpmbuild/RPMS/x86_64/${RPMNAME}
+)
+
+# userspace plugin
+(
+    VERSION=0.0.6
+    echo building userspace plugin
+    cp -r pib/libpib libpib-${VERSION}
+    mkdir -p ${HOME}/rpmbuild/SOURCES/
+    tar czvf ${HOME}/rpmbuild/SOURCES/libpib-${VERSION}.tar.gz \
+	libpib-${VERSION}/
+    rpmbuild -bs pib/libpib/libpib.spec
+
+    SRPMNAME=libpib-${VERSION}-1.el7.centos.src.rpm
+    rpmbuild --rebuild ${HOME}/rpmbuild/SRPMS/${SRPMNAME}
+    RPMNAME=libpib-${VERSION}-1.el7.centos.x86_64.rpm
+    sudo rpm -ihv ${HOME}/rpmbuild/RPMS/x86_64/${RPMNAME}
+)
