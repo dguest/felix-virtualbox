d66b22f4da1c1f3a0e2b5edfa362ec1e01b2b21f
Dan Guest <dguest@cern.ch>
2015-10-11 11:46:37 +0200

    minor

--- bootstrap/local_git.sh
+++ bootstrap/local_git.sh
@@ -4,4 +4,5 @@ set -eu
 ssh-keyscan -H github.com >> .ssh/known_hosts
 git config --global user.email "dguest@cern.ch"
 git config --global user.name "Daniel Guest"
-
+git config --global color.ui auto
+git config --global push.default simple
